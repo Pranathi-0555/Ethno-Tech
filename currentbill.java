3a8ad996371cfee454c57cb62199bb0b30972433
Pranathi-0555 <pranathib555@gmail.com>
2026-05-27 11:01:05 +0530

    Add files via upload

---
+++ currentbill.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class currentbill {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter units: ");
+        int units = sc.nextInt();
+
+        int bill;
+
+        if (units <= 250) {
+            bill = 0;
+        } else {
+            bill = (units - 250) * 2;
+        }
+
+        System.out.println("Electricity Bill = ₹" + bill);
+    }
+}
